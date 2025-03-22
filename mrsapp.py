e97638339b33a0d9c890745893a2fe11748eedf3
Hammid bin aejaz <hammidbinaejaz@Hammids-MacBook-Air.local>
2025-03-22 16:07:38 +0530

    Added main project files

---
+++ mrsapp.py
@@ -0,0 +1,48 @@
+import pickle
+import streamlit as st
+import requests
+
+# Load movie data
+movies = pickle.load(open('/Users/hammidbinaejaz/Desktop/movies.pkl', 'rb'))
+similarity = pickle.load(open('/Users/hammidbinaejaz/Desktop/similarity.pkl', 'rb'))
+
+# Function to fetch movie poster
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        if 'poster_path' in data and data['poster_path']:
+            return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    return "https://via.placeholder.com/500x750?text=No+Image"
+
+# Function to recommend movies
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+    
+    return recommended_movie_names, recommended_movie_posters
+
+# Streamlit UI
+st.header('Movie Recommender System')
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(recommended_movie_names[i])
+            st.image(recommended_movie_posters[i])
+
